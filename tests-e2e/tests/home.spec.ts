3bfe6e099738fd2c9c8e3760a7f0ae1264cb2e42
FaceID Architect <architect@faceid.local>
2026-06-22 13:18:39 +0700

    tool test dự án

---
+++ tests-e2e/tests/home.spec.ts
@@ -0,0 +1,14 @@
+﻿import { test, expect } from '@playwright/test';
+
+test.describe('Trang chủ GymFit', () => {
+  test('hiển thị tiêu đề trang', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveTitle(/GymFit|gym|fit/i);
+  });
+
+  test('có thể điều hướng menu chính', async ({ page }) => {
+    await page.goto('/');
+    const nav = page.locator('nav, header, .navbar');
+    await expect(nav.first()).toBeVisible();
+  });
+});
